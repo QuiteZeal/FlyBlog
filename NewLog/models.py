f31bfcd2d06a9b1805049277b92cf6a15f4070ec
Zeal <zeal@quitezeal.com>
2020-09-23 22:20:20 +0800

    use string replace text

--- NewLog/models.py
+++ NewLog/models.py
@@ -65,8 +65,8 @@ class Category(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
-    body = db.Column(db.String(204800))
-    body_html = db.Column(db.String(204800))
+    body = db.Column(db.String(16000000))
+    body_html = db.Column(db.String(16000000))
     # length = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
 
