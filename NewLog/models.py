c2d6d2d48ebd81e7a4de3b6287d998b937f8cb4a
Zeal <zeal@quitezeal.com>
2020-09-23 23:25:52 +0800

    return text

--- NewLog/models.py
+++ NewLog/models.py
@@ -65,8 +65,8 @@ class Category(db.Model):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
-    body = db.Column(db.String(100000))
-    body_html = db.Column(db.String(200000))
+    body = db.Column(db.Text())
+    body_html = db.Column(db.Text())
     # length = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
 
