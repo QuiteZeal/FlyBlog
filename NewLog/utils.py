874a8ebffa40bacec4843ce7ba5ed8b3e96b5bdd
Zeal <zeal@quitezeal.com>
2020-09-30 13:29:39 +0800

    add: <sup> <sub>.

--- NewLog/utils.py
+++ NewLog/utils.py
@@ -43,9 +43,9 @@ def redirect_back(default='blog.index', **kwargs):
 
 def on_changed_body(target, value, oldvalue, initiator):
     allowed_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'br', 'strong', 'em', 'b',
-                    'del', 'ul', 'ol', 'li', 'blockquote', 'pre', 'code', 'img',
+                    'del', 'ul', 'ol', 'li', 'blockquote', 'pre', 'code', 'img', 'sup', 'sub',
                     'a', 'abbr', 'span', 'div', 'table', 'thead', 'tbody', 'tr', 'th', 'td']
-    allowed_attributes = ['src', 'title', 'alt', 'href', 'class', 'sup', 'sub']
+    allowed_attributes = ['src', 'title', 'alt', 'href', 'class' ]
     html = markdown(value, output_format='html',
                     extensions=['tables', 'fenced_code', 'codehilite', 'nl2br', 'md_in_html'])
     clean_html = clean(html, tags=allowed_tags, attributes=allowed_attributes)
