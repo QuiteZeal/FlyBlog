c48eea368912f966bc51a409b37df974172ba5f2
Zeal <zeal@quitezeal.com>
2020-09-30 14:15:19 +0800

    fix: post hide.

--- NewLog/blueprints/blog.py
+++ NewLog/blueprints/blog.py
@@ -27,7 +27,10 @@ def index():
     # return render_template('blog/index.html', posts=posts)
     page = request.args.get('page', 1, type=int)
     per_page = current_app.config['BLOG_POST_PER_PAGE']
-    pagination = Post.query.filter_by(private=False).order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
+    if current_user.is_authenticated:
+        pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page,per_page=per_page)
+    else:
+        pagination = Post.query.filter_by(private=False).order_by(Post.timestamp.desc()).paginate(page, per_page=per_page)
     posts = pagination.items
     return render_template('blog/index.html', pagination=pagination, posts=posts)
 
