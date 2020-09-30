60e3ee4d5c84b7075458fc89e5c014c9043160c9
Zeal <zeal@quitezeal.com>
2020-09-30 15:42:56 +0800

    add: post slug.

--- NewLog/utils.py
+++ NewLog/utils.py
@@ -46,9 +46,9 @@ def on_changed_body(target, value, oldvalue, initiator):
     allowed_tags = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'br', 'strong', 'em', 'b',
                     'del', 'ul', 'ol', 'li', 'blockquote', 'pre', 'code', 'img', 'sup', 'sub',
                     'a', 'abbr', 'span', 'div', 'table', 'thead', 'tbody', 'tr', 'th', 'td']
-    allowed_attributes = ['src', 'title', 'alt', 'href', 'class' ]
+    allowed_attributes = ['src', 'title', 'alt', 'href', 'class']
     html = markdown(value, output_format='html',
-                    extensions=['tables', 'fenced_code', 'codehilite', 'nl2br', 'md_in_html'])
+                    extensions=['tables', 'fenced_code', 'codehilite', 'nl2br'])
     clean_html = clean(html, tags=allowed_tags, attributes=allowed_attributes)
     target.body_html = clean_html
 
